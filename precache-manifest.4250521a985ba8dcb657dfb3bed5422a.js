5dde26e2a5cd6ae314b02c81c6770e8b381ba649
Marija Marin <marijamarin2508@gmail.com>
2020-09-24 16:25:46 +0200

    Updates

---
+++ precache-manifest.4250521a985ba8dcb657dfb3bed5422a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee00efe9931ad4bf16d2fa4d968b7a5",
+    "url": "/project-app/index.html"
+  },
+  {
+    "revision": "fe7ebfccafdd04e3ebb4",
+    "url": "/project-app/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "00bd8ef9221956f74fe5",
+    "url": "/project-app/static/css/main.50e37807.chunk.css"
+  },
+  {
+    "revision": "fe7ebfccafdd04e3ebb4",
+    "url": "/project-app/static/js/2.f3f38dec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project-app/static/js/2.f3f38dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00bd8ef9221956f74fe5",
+    "url": "/project-app/static/js/main.e143abdc.chunk.js"
+  },
+  {
+    "revision": "4bca990020c886fc1817",
+    "url": "/project-app/static/js/runtime-main.f15dae1d.js"
+  },
+  {
+    "revision": "1d801dd5a6a5e44b7503e433b3a540b5",
+    "url": "/project-app/static/media/nasa.1d801dd5.jpg"
+  }
+]);
